f1827661a7f0c6fd135b074e02625bf0e5f033fd
Aditya Soni <adityasoni5859@gmail.com>
2019-05-08 13:06:22 +0530

    code refactor: removed unnecessary code and start afresh.

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image crop Example'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Something'),
+      ),
+    );
+  }
+}
