2cef832808068c8ab91fa8fe9175143622c157fb
Aditya Soni <adityasoni5859@gmail.com>
2019-05-08 15:42:08 +0530

    feature : applied image_cropper plugin along with image_picker.

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,89 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_image_crop/image_picker_handler.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>
+    with TickerProviderStateMixin, ImagePickerListener {
+  File _image;
+  AnimationController _controller;
+  ImagePickerHandler imagePickerHandler;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    imagePickerHandler = ImagePickerHandler(this, _controller);
+    imagePickerHandler.init();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Image crop Example'),
-        centerTitle: true,
+        title: Text(
+          'Image Picker and Cropper',
+        ),
       ),
-      body: Center(
-        child: Text('Something'),
+      body: GestureDetector(
+        onTap: () {
+          imagePickerHandler.showDialog(context);
+        },
+        child: Center(
+          child: (_image == null)
+              ? Card(
+                  child: Container(
+                    height: 200,
+                    width: 130,
+                    child: Center(
+                      child: Icon(
+                        Icons.add,
+                        color: Colors.pinkAccent,
+                      ),
+                    ),
+                  ),
+                )
+              : ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  child: Container(
+                    height: 200,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage(_image.path),
+                        fit: BoxFit.cover,
+                      ),
+                      border: Border.all(color: Colors.black54, width: 1.0),
+                    ),
+                  ),
+                ),
+        ),
       ),
     );
+  }
+
+//
+  @override
+  userImage(File _image) {
+    setState(() {
+      this._image = _image;
+    });
   }
 }
---
+++ lib/image_picker_handler.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_image_crop/image_picker_dialog.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerHandler {
+  ImagePickerDialog imagePickerDialog;
+  AnimationController _controller;
+  ImagePickerListener _listener;
+
+  ImagePickerHandler(this._listener, this._controller);
+
+  openCamera() async {
+    imagePickerDialog.dismissDialog();
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    cropImage(image);
+  }
+
+  openGallery() async {
+    imagePickerDialog.dismissDialog();
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    cropImage(image);
+  }
+
+  void init() {
+    imagePickerDialog = new ImagePickerDialog(this, _controller);
+    imagePickerDialog.initState();
+  }
+
+  Future cropImage(File image) async {
+    //Cropping image in size 1024x1024 since 1MB == 1024KB
+    //Just making a wild guess while cropping
+    File croppedFile = await ImageCropper.cropImage(
+      sourcePath: image.path,
+      ratioX: 1.0,
+      ratioY: 1.0,
+      maxWidth: 1024,
+      maxHeight: 1024,
+    );
+    _listener.userImage(croppedFile);
+  }
+
+  showDialog(BuildContext context) {
+    imagePickerDialog.getImage(context);
+  }
+}
+
+abstract class ImagePickerListener {
+  userImage(File _image);
+}
